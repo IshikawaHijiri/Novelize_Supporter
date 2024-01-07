b7989d8dc8924d0d3afe2ea26ffee1f8a2fa960f
Ishikawa <ishikawahijiri2@gmail>
2024-01-07 17:40:54 +0900

    make some files

---
+++ aaa.py
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:3856d50b6a5e3c2588477a5a38ed7616eb7bd66803a0ed90612d32e324836015
+size 639
---
+++ app.py
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:00a926ea4de5921713ba09534ef7c4b18b8b49abce49d430b38ac66ac9477823
+size 3852
