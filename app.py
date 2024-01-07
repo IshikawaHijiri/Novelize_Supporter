927cc18a5715404b2581fe21806fac417fdd1b57
Ishikawa <ishikawahijiri2@gmail>
2024-01-07 20:01:52 +0900

    change comment

--- app.py
+++ app.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:00a926ea4de5921713ba09534ef7c4b18b8b49abce49d430b38ac66ac9477823
-size 3852
+oid sha256:e687cc6e9b8c3d6708812f02946b71d284e69b9a8a84454e3a284254d086bb66
+size 3862
