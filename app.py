c4319a69957cb0bbf7d6ab21a997e26d6220de6e
Ishikawa <ishikawahijiri2@gmail>
2024-02-21 11:03:11 +0900

    fix

--- app.py
+++ app.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:bda3c7238a6b2f835cedaab85f07bcf6f81407f0b76d801a9cb99eab68d42f3c
-size 4010
+oid sha256:83c8ff668825ee30115108e99f7c1d4cac12f9478849ff1790b50d70e40d34d5
+size 4009
